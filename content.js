8a7c26473590e4f64edf6b8deda0168549af83c4
rustleupchef <rustle.up.chef@gmail.com>
2025-10-19 10:25:55 -0500

    final product

--- content.js
+++ content.js
@@ -1,14 +1,15 @@
 browser.runtime.onConnect.addListener(port => {
     if (port.name === "popup-port") {
         port.onMessage.addListener(msg => {
-            const options = document.querySelectorAll(".mcq-option");
-            let sep = []
-            for(let i = 0; i < options.length; i++) {
-                if (options[i].ariaLabel.startsWith("Option A")) {
+            const options = Array.from(document.querySelectorAll(".mcq-option"));
+            let sep = [];
+            options.forEach(option => {
+                const option1 = option.cloneNode(true);
+                if (option1.ariaLabel.startsWith("Option A")) {
                     sep.push([]);
                 }
-                sep[sep.length - 1].push(options[i]);
-            }
+                sep[sep.length - 1].push(option1);
+            });
             const questions = document.querySelectorAll(".lrn_question");
 
             let csvTxt = "";
@@ -18,7 +19,7 @@ browser.runtime.onConnect.addListener(port => {
                 let optionsSet = sep[i];
 
                 for (let j = 0; j < optionsSet.length; j++) {
-                    if (optionsSet[j].classList.contains("--correct")) {
+                    if (optionsSet[j].children[2].children[0].classList.contains("--correct")) {
                         set = optionsSet.splice(j, 1);
                     }
                 }
@@ -26,11 +27,34 @@ browser.runtime.onConnect.addListener(port => {
                 for (let j = 0; j < optionsSet.length && j < 3; j++) {
                     set.push(optionsSet[j]);
                 }
+
+                set = shuffleArray(set);
+
+                let index = 1;
+                for (let j = 0; j < set.length; j++) {
+                    if (set[j].children[2].children[0].classList.contains("--correct")) {
+                        index = j + 1;
+                    }
+                }
                 
-                csvTxt += `"${questions[i].innerText}","${set[0] ? set[0].innerText.substring(1) : ""}","${set[1] ? set[1].innerText.substring(1) : ""}","${set[2] ? set[2].innerText.substring(1) : ""}","${set[3] ? set[3].innerText.substring(1) : ""}",20,"${1}"\n`
+                csvTxt += `"${questions[i].innerText}","${set[0] ? set[0].innerText.substring(1) : ""}","${set[1] ? set[1].innerText.substring(1) : ""}","${set[2] ? set[2].innerText.substring(1) : ""}","${set[3] ? set[3].innerText.substring(1) : ""}",20,"${index}"\n`
             }
 
             port.postMessage({response: csvTxt});
         });
     }
 });
+
+function shuffleArray(array) {
+  let currentIndex = array.length;
+  let randomIndex;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+}
